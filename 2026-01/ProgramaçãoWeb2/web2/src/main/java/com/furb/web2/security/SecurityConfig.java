e0be1450ce31e99b99dd5b2f5635313df69b9493
Thomas Leon Rodrigues <galanislyan@gmail.com>
2026-06-10 18:46:53 -0300

    Add files via upload

---
+++ 2026-01/ProgramaçãoWeb2/web2/src/main/java/com/furb/web2/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.furb.web2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http
+        .csrf(csrf -> csrf.disable())
+        .headers(headers ->
+            headers.frameOptions(frame -> frame.disable())
+        )
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/h2-console/**"
+            ).permitAll()
+            .anyRequest().permitAll()
+        );
+
+    return http.build();
+}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
